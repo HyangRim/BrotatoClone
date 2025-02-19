094a3e5f534328d97da938086f34b42c0c34ea7e
Park in su <eyiuta1@gmail.com>
2025-02-20 03:51:06 +0900

    WEAPON 생성중...

---
+++ Client/CWeapon.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "CWeapon.h"
+
+CWeapon::CWeapon()
+	: m_tWeaponInfo{}
+	, m_vRotation()
+{
+}
+
+CWeapon::~CWeapon()
+{
+
+
+}
+
---
+++ Client/Unity/unity_A1GORLSHVATE2SG4.cpp
@@ -0,0 +1,33 @@
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CResMgr.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CRigidbody.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CScene.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CSceneMgr.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CScene_Start.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CScene_Test.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CScene_Tool.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CSound.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CSoundMgr.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CState.cpp"
+
+
+#include "C:\Users\HyangRim\source\repos\BrotatoClone\Client\CTexture.cpp"
+
