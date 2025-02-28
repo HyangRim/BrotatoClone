44a9efcac32455833b83797d8e84fe4c2d80cda9
Park in su <eyiuta1@gmail.com>
2025-02-28 10:17:48 +0900

    상수 일단 주석으로 넣기.

--- Client/CharacterInfoMgr.h
+++ Client/CharacterInfoMgr.h
@@ -1,5 +1,7 @@
 #pragma once
 
+
+
 struct tCharacterInfo
 {
 	wstring         m_sName;			// 플레이어 이름. 
@@ -15,6 +17,34 @@ struct tCharacterInfo
 	float			m_iCriticalAcc;		// 크확 %
 	float			m_fSpeed;			// 속도 계수.
 };
+
+/*
+tCharacterInfo NormalInfo = {
+	NormalInfo.m_sName = L"다재다능",
+	NormalInfo.m_eType = CHAR_TYPE::WELL_ROUNDED,
+	NormalInfo.m_fDefaultSpeed = 100.f,
+	NormalInfo.m_AddMaxHP = 5,
+	NormalInfo.m_fDamageCoef = 0.f,
+	NormalInfo.m_fMeleeCoef = 1.f,
+	NormalInfo.m_fRangeCoef = 1.f,
+	NormalInfo.m_fAttackSpeedCoef = 1.f,
+	NormalInfo.m_iCriticalAcc = 1.f,
+	NormalInfo.m_fSpeed = 1.f
+};
+
+tCharacterInfo NormalInfo = {
+	NormalInfo.m_sName = L"레인저",
+	NormalInfo.m_eType = CHAR_TYPE::RANGER,
+	NormalInfo.m_fDefaultSpeed = 80.f,
+	NormalInfo.m_AddMaxHP = 0,
+	NormalInfo.m_fDamageCoef = 0.f,
+	NormalInfo.m_fMeleeCoef = 1.f,
+	NormalInfo.m_fRangeCoef = 1.5f,
+	NormalInfo.m_fAttackSpeedCoef = 1.f,
+	NormalInfo.m_iCriticalAcc = 1.f,
+	NormalInfo.m_fSpeed = 1.f
+};
+*/
 
 class CharacterInfoMgr
 {
